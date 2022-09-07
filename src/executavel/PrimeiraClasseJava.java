a73a15b852bb7ea42aab669a9fd1a1c246ea5819
Stephanie <sthersousa07@gmail.com>
2022-09-06 21:23:19 -0300

    criação novas variaves

--- src/executavel/PrimeiraClasseJava.java
+++ src/executavel/PrimeiraClasseJava.java
@@ -5,7 +5,21 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 	
-
-		System.out.println("luciano");
+    int maiorIdade = 18;
+    
+    int idadeIdoso = 60;
+    
+    int idadeJovem = 16;
+    		
+    int idadeCrianca = 7;
+    
+    String cpf = "082.021.184-39";
+    
+   System.out.println("Maior idade é = " + maiorIdade);
+   System.out.println("Melhor idade é = " + idadeIdoso);
+   System.out.println("Jovem idade é = " + idadeJovem);
+   System.out.println("Criança idade é = " + idadeCrianca);
+   System.out.println("O CPF É = " + cpf + " LUCIANO");
+    
 	}
 }
