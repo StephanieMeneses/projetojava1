ca5703b7f3caabe9263bbc1deda4258d904bfa17
Stephanie <sthersousa07@gmail.com>
2022-09-06 21:51:37 -0300

    criação do metodo 2

--- src/executavel/PrimeiraClasseJava.java
+++ src/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,8 @@ package executavel;
 
 public class PrimeiraClasseJava {
 
-	
+	static int maiorIdade = 30;
+
 	public static void main(String[] args) {
 	
     int maiorIdade = 18;
@@ -20,6 +21,17 @@ public class PrimeiraClasseJava {
    System.out.println("Jovem idade é = " + idadeJovem);
    System.out.println("Criança idade é = " + idadeCrianca);
    System.out.println("O CPF É = " + cpf + " LUCIANO");
-    
+   
+   metodo2(cpf, idadeIdoso, idadeJovem, idadeCrianca); 
+   
 	}
+	  public static void metodo2(String cpfLuciano, int idadeIdoso, int idadeJovem, int idadeCrianca) {
+      System.out.println("Maior idade é = " + maiorIdade);
+      System.out.println("O CPF É = " + cpfLuciano + " LUCIANO");
+
+	  }
+	  public static void metodo2(String cpfLuciano) {
+		  
+	  }
+
 }
