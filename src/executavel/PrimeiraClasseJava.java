694e14edd04c5acd77d19c353608dacf55bb48e9
LUCIANO ARAUJO MENESES - GETIC - GETIC <214083@int.cagece.com.br>
2022-09-10 21:34:20 -0300

    modulo 7 - operações lógicas

--- src/executavel/PrimeiraClasseJava.java
+++ src/executavel/PrimeiraClasseJava.java
@@ -2,24 +2,62 @@ package executavel;
 
 public class PrimeiraClasseJava {
 
-	
 	public static void main(String[] args) {
-	
-    int maiorIdade = 18;
-    
-    int idadeIdoso = 60;
-    
-    int idadeJovem = 16;
-    		
-    int idadeCrianca = 7;
-    
-    String cpf = "082.021.184-39";
-    
-   System.out.println("Maior idade é = " + maiorIdade);
-   System.out.println("Melhor idade é = " + idadeIdoso);
-   System.out.println("Jovem idade é = " + idadeJovem);
-   System.out.println("Criança idade é = " + idadeCrianca);
-   System.out.println("O CPF É = " + cpf + " LUCIANO");
-    
+
+		int nota1 = 7;
+		int nota2 = 7;
+		int nota3 = 7;
+		int nota4 = 7;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		/* Condições lógicas com IF e ELSE */
+
+		/*
+		 * if (media >= 7) { System.out.println("Aluno aprovado: " + media); } else if
+		 * (media >= 4 && media <= 6) { System.out.println("Aluno em recuperação " +
+		 * media); } else { System.out.println("Você foi reprovado: " + media);
+		 * 
+		 * }/*
+		 * 
+		 * /* Operadores ternários são para micro validações
+		 */
+
+		String saidaResultado;
+
+		/*
+		 * saidaResultado = media >= 7 ? "Aluno Aprovado" : (media >= 4 && media <= 6) ?
+		 * "Aluno em Recuperação" : "Aluno Reprovado";
+		 * 
+		 * System.out.println(saidaResultado);*/
+
+		/*if (media < 5) {
+			if (media >= 7) {
+				if (media > 9) {
+					System.out.println("Aluno aprovado direto - Parabéns");
+				} else {
+					System.out.println("Aluno está aprovado direto " + media );
+				}
+				System.out.println("Aluno está em recuperação");
+			}
+		} else {
+			System.out.println("Aluno Reprovado Direto " + media);
+		} */
+		
+		/*SWITH CASE: OPERAÇÕES EXATAS*/
+		int dia = 2;  
+		switch (dia) {
+		case 1:
+			System.out.println("domingo");
+			
+			break;
+		case 2:
+			System.out.println("Segunda-Feira");
+			break; 
+
+		default: System.out.println("outro dia qualquer ");
+			break;
+		}
 	}
 }
